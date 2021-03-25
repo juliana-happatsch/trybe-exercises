25aa5f4f393ee388486db7a7824148701ff7c67f
Juliana Happatsch <jhappatsch@gmail.com>
2021-03-24 22:46:20 -0300

    Passo 6

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/xadrez.js
@@ -0,0 +1,42 @@
+let piece = prompt('Digite o nome da peça de xadrez:'); 
+let lowerCase = piece.toLowerCase();
+
+switch (lowerCase) {
+    case 'peão':
+        console.log('Para frente (captura peças na diagonal)');
+        break;
+
+    case 'rei':
+        console.log('Vertical, horizontal e diagonal');
+        break;
+
+    case 'rainha':
+        console.log('Vertical, horizontal e diagonal');
+        break;
+
+    case 'torre':
+        console.log('Linha reta na vertical ou horizontal');
+        break;
+
+    case 'bispo':
+        console.log('Diagonal');
+        break;
+
+    case 'cavalo':
+        console.log('Em L');
+        break;
+
+    default:
+        console.log('Peça inválida');
+}
+
+console.log(piece);
+
+/*
+Rei
+Rainha
+Torre
+Bispo
+Cavalo
+Peão
+*/
