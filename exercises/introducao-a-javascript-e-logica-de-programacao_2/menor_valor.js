5238cd40cbabd0ea30aa55f73e108860933d47ac
Juliana Happatsch <jhappatsch@gmail.com>
2021-03-25 19:14:32 -0300

    Exercício 7 e corrigindo exercicio 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/menor_valor.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/menor_valor.js
@@ -1,10 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let max = numbers[0];
+let min = numbers[0];
 
 for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] > max) {
-        max = numbers[i];
+    if (numbers[i] < min) {
+        min = numbers[i];
     }
 }
 
-console.log(max);
+console.log(min);
